68700f1f155264fbba52cf2b1adb0c04c3faac20
pchoudhary7817-max <pchoudhary7817@gmail.com>
2026-03-24 22:24:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,37 @@
-<html><head><meta content="text/html; charset=UTF-8" http-equiv="content-type"><title>Gmail - Download error</title></head><body>For security reasons, Gmail does not allow you to use this type of file as it violates Google policy for executables and archives.</body></html>
+const express = require('express');
+const fs = require('fs');
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(express.static('public'));
+
+// Get products
+app.get('/products', (req, res) => {
+  const data = JSON.parse(fs.readFileSync('db.json'));
+  res.json(data.products);
+});
+
+// Add product
+app.post('/add-product', (req, res) => {
+  const data = JSON.parse(fs.readFileSync('db.json'));
+  data.products.push(req.body);
+  fs.writeFileSync('db.json', JSON.stringify(data, null, 2));
+  res.json({ message: "Product Added" });
+});
+
+// Place order
+app.post('/order', (req, res) => {
+  const data = JSON.parse(fs.readFileSync('db.json'));
+  data.orders.push(req.body);
+  fs.writeFileSync('db.json', JSON.stringify(data, null, 2));
+  res.json({ message: "Order Placed" });
+});
+
+// IMPORTANT: Render ke liye correct port
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
