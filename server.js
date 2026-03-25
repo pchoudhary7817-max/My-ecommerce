1a2b081244f6c5ee13cb346546b93cf51c6cd128
pchoudhary7817-max <pchoudhary7817@gmail.com>
2026-03-25 10:17:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -28,4 +28,7 @@ app.post('/order', (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
+app.get('/', (req, res) => {
+  res.send('Server is running 🚀');
+});
 app.listen(PORT, () => console.log("Server running on port " + PORT));
